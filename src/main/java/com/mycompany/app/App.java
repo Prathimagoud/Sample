b5786b5024d2e194bf223777665a54cabfd7172a
Prathimagoud <109512486+Prathimagoud@users.noreply.github.com>
2023-02-20 10:37:17 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -15,9 +15,9 @@ public class App
         
         try{
             System.out.println("myObject: " + myObject.toString());
-        }catch(Exception e){
-            
         }
+            
+        
             
     }
 }
